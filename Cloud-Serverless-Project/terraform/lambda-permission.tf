230f31e94c13d2f8c28f2ea05c6f3fcc961b648b
mingyu110 <48540798+mingyu110@users.noreply.github.com>
2024-08-07 11:05:21 +0800

    Update lambda-permission.tf

--- Cloud-Serverless-Project/terraform/lambda-permission.tf
+++ Cloud-Serverless-Project/terraform/lambda-permission.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_permission" "apigw2" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.POST_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/POST/addStudent"
+  source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/PUT/addStudent"
 
   depends_on = [aws_api_gateway_deployment.student_api_deployment]
 }
